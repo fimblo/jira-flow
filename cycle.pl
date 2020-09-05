21fb9ed2e444737cb3c9ce2974b8de63e6482322
Mattias Jansson <fimblo@yanson.org>
2020-09-05 21:01:44 +0200

    take bank holidays into consideration

--- cycle.pl
+++ cycle.pl
@@ -7,12 +7,28 @@ use strict;
 use JSON;
 use Class::Date qw(:errors date);
 
+# put all holidays into a hash
+my $holidays;
+for my $h (
+           qw(2020/01/01 2020/01/06 2020/04/10 2020/04/13 2020/05/01 2020/05/21
+              2020/06/19 2020/12/24 2020/12/25 2020/12/31
+
+              2021/01/01 2021/01/06 2021/04/02 2021/04/05 2021/05/13 2021/06/25
+              2021/12/24 2021/12/31 )
+          ) {
+  $holidays->{$h} = 1;
+}
+
+# --------------------------------------------------
+# Start here
 my $input = decode_json(join('', <STDIN> ));
 my @issues = @{$input->{issues}};
 
 my $number_of_tickets = 0;
 my $total_cycle_time;
 
+
+# loop through each jira issue
 for my $issue (@issues) {
 
   my $key = $issue->{key};
@@ -40,18 +56,27 @@ for my $issue (@issues) {
   my $doneTimeReal = date $done_s;
   my $doneTime = date($doneTimeReal);
 
-  # check for weekends
+  # check for weekends and holidays
   my $weekendCount = 0;
+  my $holidayCount = 0;
   my $day = $startTime;
   my $lastDay = $doneTime->ymd;
   while ($day->ymd ne $lastDay) {
     $day = $day+'1D';
     my $wday = $day->wday;
 
-    if ($wday == 1 or $wday == 7) { # Sunday or Saturday;
+    # skip if Sunday(1) or Saturday(7)
+    if ($wday == 1 or $wday == 7) {
       $doneTime = $doneTime-'1D';
       $weekendCount++;
     }
+
+    # skip if this is a holiday
+    if (exists $holidays->{$day->ymd}) {
+      $doneTime = $doneTime-'1D';
+      $holidayCount++;
+    }
+
   }
   my $cycleTime = $doneTime - $startTime;
 
@@ -60,6 +85,7 @@ for my $issue (@issues) {
   print ' ' x 5 . "Start: " . $startTime->ymd . ' ' . $startTime->hms . "\n";
   print ' ' x 5 . "  End: " . $doneTimeReal->ymd . ' ' . $doneTimeReal->hms . "\n";
   print ' ' x 5 . "There are $weekendCount off-days in this range.\n";
+  print ' ' x 5 . "There are $holidayCount holidays in this range.\n";
   print ' ' x 5 . "Cycle: " . $cycleTime->hour . "\n";
 
   $total_cycle_time += $cycleTime->hour;
